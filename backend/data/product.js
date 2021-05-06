d81ff29da10f8fb818683b575d37868ca801e159
NavodyaPasqual <68048593+NavodyaPasqual@users.noreply.github.com>
2021-05-06 18:58:16 +0530

    add frontend

---
+++ backend/data/product.js
@@ -0,0 +1,12 @@
+const products = [
+
+    {
+        name: "",
+        imageUrl: " ",
+        description: " ",
+        price: 0,
+        countInStock: 0,
+    },
+];
+
+module.exports = products;
